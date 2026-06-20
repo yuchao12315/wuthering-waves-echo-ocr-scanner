28a561de57b80edf4b8a7b4589ad9a8f6c0f260a
qingmeng <1>
2026-06-20 22:07:43 +0800

    fix：固定端口 防止数据丢失

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  server: {
+    port: 9998,
+    strictPort: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
